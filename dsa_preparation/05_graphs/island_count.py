3bff2381a444b9ba9ba53194a407c652cb7d31db
Kaushik Kishore <kk12391@gmail.com>
2025-03-31 12:05:27 +0530

    fixed iland count for the dfs and bfs

--- dsa_preparation/05_graphs/island_count.py
+++ dsa_preparation/05_graphs/island_count.py
@@ -14,12 +14,12 @@
   ```
 
 """
-
+from collections import deque
 def island_count(board):
-    row, col = len(board), len(board[0])
+    rows, cols = len(board), len(board[0])
 
     def dfs(r, c):
-        if r < 0 or r >= row or c < 0  or c >= col or board[r][c] == '0':
+        if r < 0 or r >= rows or c < 0  or c >= cols or board[r][c] == '0':
             return
 
         board[r][c] = '0'
@@ -30,15 +30,30 @@ def island_count(board):
         dfs(r, c+1)
         dfs(r, c-1)
 
+    def bfs(r, c):
+        queue = deque()
+        queue.append((r,c))
+        board[r][c] = '0'
+
+        while queue:
+            row, col = queue.popleft()
+            for dr, dc in [(-1,0),(1,0),(0,-1),(0,1)]:
+                nr = row + dr 
+                nc = col + dc
+                if 0 <= nr < rows and 0 <= nc < cols and board[nr][nc] == "1":
+                    board[nr][nc] = "0"
+                    queue.append((nr,nc))
+
     count = 0
-    for r in range(row):
-        for c in range(col):
+    for r in range(rows):
+        for c in range(cols):
             if board[r][c] == "1":
                 count += 1
                 dfs(r, c)
 
     return count
 
+    
 
 board =   [
     ["1","1","0","0","0"],
